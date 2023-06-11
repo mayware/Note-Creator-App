6181a0ab2ef22e46d9312fbbc592a8a3adfc4d92
tofa93 <tofig.valiyev1993@gmail.com>
2023-06-12 01:08:46 +0200

    Deployment

---
+++ src/components/NewNote.tsx
@@ -0,0 +1,10 @@
+import { NoteForm } from "./NoteForm"
+
+export function NewNote() {
+    return (
+        <div className="content">
+            <h1>New note</h1>
+            <NoteForm />
+        </div>
+    )
+}
